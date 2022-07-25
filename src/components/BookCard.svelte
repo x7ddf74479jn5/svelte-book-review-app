16879a935986bba5fa7616dab7e60089e1989a00
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-07-26 02:47:18 +0900

    ✨ feat: DetailsBook page

--- src/components/BookCard.svelte
+++ src/components/BookCard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { link } from 'svelte-spa-router'
   import type { BookItem } from '../repositories/book'
   export let book: BookItem
 
@@ -17,7 +18,9 @@
   </div>
   <div class="border-r border-b border-l border-grey-light sm:border-l-0 sm:border-t sm:border-grey-light bg-white rounded-b sm:rounded-b-none sm:rounded-r p-4 flex flex-col justify-between leading-normal w-100 sm:w-9/12 lg:w-7/12">
     <div class="my-4">
-      <div class="text-black font-bold text-xl mb-2">{book.volumeInfo.title}</div>
+      <a href={`/books/${book.id}`} use:link>
+        <div class="text-black font-bold text-xl mb-2">{book.volumeInfo.title}</div>
+      </a>
       <p class="text-grey-darker text-sm break-words w-9/12 m-auto">
         {description}
       </p>
